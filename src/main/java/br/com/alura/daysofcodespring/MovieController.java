e7f061944f84fab20f5d47b5ad40c45794b040f8
Leandro Vianna <leandro97vianna@gmail.com>
2023-03-11 14:20:48 -0300

    Day 6 - Dia 6

--- src/main/java/br/com/alura/daysofcodespring/MovieController.java
+++ src/main/java/br/com/alura/daysofcodespring/MovieController.java
@@ -2,12 +2,14 @@ package br.com.alura.daysofcodespring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MovieController {
@@ -15,19 +17,28 @@ public class MovieController {
     @Autowired
     private ImdbApiClient imdbApiClient;
 
+    private List<Movie> movies;
+
     @GetMapping("/top250")
-    public ListOfMovies getTop250Movies() {
-        ListOfMovies movies = imdbApiClient.getBody();
+    public List<Movie> getTop250Movies(@RequestParam Optional<String> title) {
+        if (movies == null) {
+            ListOfMovies response = imdbApiClient.getBody();
+            movies = response.items;
+        }
+
+        final List<Movie> filteredMovies = movies.stream()
+                .filter(m -> m.title().contains(title.orElse("")))
+                .toList();
 
         try {
             Writer writer = new PrintWriter("src/main/resources/content.html");
-            new HTMLGenerator(writer).generate(movies.items);
+            new HTMLGenerator(writer).generate(filteredMovies);
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return movies;
+        return filteredMovies;
     }
 
     record ListOfMovies(List<Movie> items, String errorMessage) {}
